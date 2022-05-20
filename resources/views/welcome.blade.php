03d3e3d5fc2114f644af6f7ab57a28626f98b732
Shin <ShinHate@gmail.com>
2022-05-20 09:51:33 +0300

    Laravel add VUE 3 crm:15

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body class="antialiased">
+<div id="app">
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
+        <hello-world/>
+    </div>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
