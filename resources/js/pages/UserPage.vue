95159f9c1f52ebbba1a571fd23d40296f25bb956
Shin <ShinHate@gmail.com>
2022-05-20 17:18:25 +0300

    Laravel VUE CRM:22

---
+++ resources/js/pages/UserPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{ user.name }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserPage",
+    data() {
+        return {
+            userId: null,
+            user:{},
+        }
+    },
+    async  mounted() {
+        let userId = this.this.$route.params.id;
+        let result = await axios.get(`/users/${userId}`);
+        this.users = result.data;
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
